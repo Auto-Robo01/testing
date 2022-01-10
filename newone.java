330fabb7c6b2ca248a8cd0676857aade37b0c96a
Hemant Hawa <hawahemant7893@gmail.com>
2022-01-10 17:03:10 +0530

    initial commit

---
+++ newone.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class newone{
+    
+    static int adder(int a, int b){
+        int x = a+b;
+        return x;
+    }
+    static int sub(int a, int b){
+        int x = a-b;
+        return x;
+    }
+    static int multiplier(int a, int b){
+        int x = a*b;
+        return x;
+    }
+    static int divisor(int a, int b){
+        int x = a/b;
+        return x;
+    }
+    
+    public static void main(String[] args){
+        func1();
+        func2(69);
+    }
+}
